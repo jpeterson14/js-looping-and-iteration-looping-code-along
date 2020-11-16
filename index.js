b7441211aae7c6ea5af63aa41b29db148948fa63
jpeterson14 <jpeterson0514@gmail.com>
2020-11-16 16:25:24 -0600

    commit tree 8199c4c0f8e98aba1ba87273b7f30f2a735dae2a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let emptyArray = []
+    for (let i = 0; i < names.length; i++){
+        emptyArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return emptyArray;
+}
+function countDown(x) {
+    for (let i = x; i >= 0; i--) {
+        console.log(i)
+    }
+}
